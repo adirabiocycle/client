580fc0ce340491492969308d4b3ee1f7d39d8d75
manjeetchandra <majormchandra@gmail.com>
2025-08-18 23:11:29 +0530

    temp fix

--- src/components/contact/ContactInfo.tsx
+++ src/components/contact/ContactInfo.tsx
@@ -1,4 +1,4 @@
-import { Mail, Phone, MapPin, Clock, Briefcase } from 'lucide-react';
+import { Mail, Phone, MapPin, Clock } from 'lucide-react';
 
 export default function ContactInfo() {
   return (
